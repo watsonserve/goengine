a06c854ee466047d5b1a39991760b1941a3b53df
JamesWatson <HamishWatson@live.com>
2020-12-27 19:01:59 +0800

    example

---
+++ example_app_test.go
@@ -0,0 +1,31 @@
+package goengine_test
+
+import (
+    "fmt"
+    "net/http"
+    "github.com/watsonserve/goengine"
+)
+
+func filter (resp http.ResponseWriter, sess *Session, req *http.Request) {
+    // do something
+    return true
+}
+
+func actionFoo (resp http.ResponseWriter, sess *Session, req *http.Request) {
+    resp.Write("")
+}
+
+func actionBar (resp http.ResponseWriter, sess *Session, req *http.Request) {
+    resp.Write("")
+}
+
+func ExampleGoengine() {
+    router := goengine.InitHttpRoute()
+    router.Use(filter)
+    router.SetWith("^/foo/.+", actionFoo)
+    router.SetWith("^/bar", actionBar)
+
+    engine := goengine.New(nil)
+    engine.UseRouter(router)
+    engine.ListenTCP(":7070")
+}
